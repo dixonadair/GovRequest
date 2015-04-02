aaec018bdf8b67515023eef64c66725e316cdd67
Yumiko Huang <dayumikoda@gmail.com>
2015-04-02 00:44:59 -0700

    successfully created report and post a message by ajax after submission

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,15 @@ post '/' do
 	redirect :index
 end
 
+post '/report/create' do
+	new_report = Report.new(params[:report])
+	new_report.user_id = session[:user_id]
+	new_report.save
+	if request.xhr?
+		content_type :json
+		{data: params}.to_json
+	else
+		redirect '/'
+	end
+end
+
