74dcf6f8a15af4c766d5ea0122e59b3712dff5b1
Dixon Adair <dixonadairv@gmail.com>
2015-04-02 16:49:17 -0700

    cleaned up current_location.js file

--- public/js/current_location.js
+++ public/js/current_location.js
@@ -1,53 +1,59 @@
-$(function(){
+$(function() {
+
 	L.mapbox.accessToken = 'pk.eyJ1IjoieXVtaWtvIiwiYSI6IkRDVk43ZjAifQ.aAdCw3xUw8s1QLZzcBUhbA';
+	
 	var geolocate = $('#geolocate'),
 	    current_location_map = L.mapbox.map('current_location_map', 'yumiko.lkbilml1'),
 	    myLayer = L.mapbox.featureLayer().addTo(current_location_map);
-			current_location_map.setView([37.734585, -122.447214], 12);
 
-	var markerGenerator = function(lat, lng, report_issue, report_id){
+	current_location_map.setView([37.734585, -122.447214], 12);
+
+	var markerGenerator = function(lat, lng, report_issue, report_id) {
 		var marker = L.mapbox.featureLayer({
-	    type: 'Feature',
-	    geometry: {
-	        type: 'Point',
-	        coordinates: [parseFloat(lat), parseFloat(lng)]
-	    },
-	    properties: {
-	        title: "<a href='http://localhost:9393/reports/"+report_id+"'>" + report_issue + "</a>",
-	        'marker-color': '#f86767'
-	    }
-		}).addTo(current_location_map);
-	}
+		    type: 'Feature',
+		    geometry: {
+		        type: 'Point',
+		        coordinates: [parseFloat(lat), parseFloat(lng)]
+		    },
+		    properties: {
+		        title: "<a href='http://localhost:9393/reports/"+report_id+"'>" + report_issue + "</a>",
+		        'marker-color': '#f86767'
+		    }
+		});
+		marker.addTo(current_location_map);
+	};
 
 	var iterateReports = function(reportsData) {
 		for (var i = 0; i < 32; i++) {
 			console.log(reportsData[i].lat + " " + reportsData[i].lng);
 			markerGenerator(reportsData[i].lat, reportsData[i].lng, reportsData[i].report_name,reportsData[i].id);
-	  }
-	}
+	  	}
+	};
 
-  var list_of_reports = function(){
-	$.ajax({
+  	var list_of_reports = function() {
+		var ajaxRequest = $.ajax({
 			url: '/reports/show',
-			type: 'GET',
-		}).done(function(response){
+			type: 'GET'
+		});
+		ajaxRequest.done(function(response) {
 			iterateReports(response.collection);
-		}).fail(function(error){
+		});
+		ajaxRequest.fail(function(error) {
 			console.log('fail');
 		});
-	}
+	};
 
-  list_of_reports();
+  	list_of_reports();
 
 	if (!navigator.geolocation) {
 	    geolocate.html('Geolocation is not available');
 	} else {
-			geolocate.on('click',function(e){
-				e.preventDefault();
-				e.stopPropagation();
-				current_location_map.locate();
-			});
-	}
+		geolocate.on('click',function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			current_location_map.locate();
+		});
+	};
 
 	current_location_map.on('locationfound', function(e) {
 	    current_location_map.fitBounds(e.bounds);
@@ -74,6 +80,5 @@ $(function(){
 	current_location_map.on('locationerror', function() {
 	    geolocate.html('Position could not be found');
 	});
-
 
 });
