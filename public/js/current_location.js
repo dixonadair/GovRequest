b115be95e8c4351366e497fb395a64607c180c1f
Yumiko Huang <dayumikoda@gmail.com>
2015-04-01 23:01:13 -0700

    create report form successfully get user's current Lat and Lng

--- public/js/current_location.js
+++ public/js/current_location.js
@@ -1,48 +1,43 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoieXVtaWtvIiwiYSI6IkRDVk43ZjAifQ.aAdCw3xUw8s1QLZzcBUhbA';
-var geolocate = document.getElementById('geolocate');
-var map = L.mapbox.map('map', 'yumiko.lkbilml1');
+$(function(){
+	L.mapbox.accessToken = 'pk.eyJ1IjoieXVtaWtvIiwiYSI6IkRDVk43ZjAifQ.aAdCw3xUw8s1QLZzcBUhbA';
+	var geolocate = $('#geolocate');
+	var current_location_map = L.mapbox.map('current_location_map', 'yumiko.lkbilml1');
+	var myLayer = L.mapbox.featureLayer().addTo(current_location_map);
 
-var myLayer = L.mapbox.featureLayer().addTo(map);
+	if (!navigator.geolocation) {
+	    geolocate.html('Geolocation is not available');
+	} else {
+			geolocate.on('click',function(e){
+				e.preventDefault();
+				e.stopPropagation();
+				current_location_map.locate();
+			});
+	}
 
-// This uses the HTML5 geolocation API, which is available on
-// most mobile browsers and modern browsers, but not in Internet Explorer
-//
-// See this chart of compatibility for details:
-// http://caniuse.com/#feat=geolocation
-if (!navigator.geolocation) {
-    geolocate.innerHTML = 'Geolocation is not available';
-} else {
-    geolocate.onclick = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        map.locate();
-    };
-}
+	current_location_map.on('locationfound', function(e) {
+	    current_location_map.fitBounds(e.bounds);
 
-// Once we've got a position, zoom and center the map
-// on it, and add a single marker.
-map.on('locationfound', function(e) {
-    map.fitBounds(e.bounds);
+	    var user_geo_data = myLayer.setGeoJSON({
+	        type: 'Feature',
+	        geometry: {
+	            type: 'Point',
+	            coordinates: [e.latlng.lng, e.latlng.lat]
+	        },
+	        properties: {
+	            'title': 'Here I am!',
+	            'marker-color': '#ff8888',
+	            'marker-symbol': 'star'
+	        }
+	    });
 
-    myLayer.setGeoJSON({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [e.latlng.lng, e.latlng.lat]
-        },
-        properties: {
-            'title': 'Here I am!',
-            'marker-color': '#ff8888',
-            'marker-symbol': 'star'
-        }
-    });
+	    $('#user_current_lat').val(user_geo_data._geojson.geometry.coordinates[0]);
+	    $('#user_current_lng').val(user_geo_data._geojson.geometry.coordinates[1]);
 
-    // And hide the geolocation button
-    geolocate.parentNode.removeChild(geolocate);
-});
+	    geolocate.remove();
+	});
 
-// If the user chooses not to allow their location
-// to be shared, display an error message.
-map.on('locationerror', function() {
-    geolocate.innerHTML = 'Position could not be found';
+	current_location_map.on('locationerror', function() {
+	    geolocate.html('Position could not be found');
+	});
+
 });
