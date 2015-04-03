ad793eeddddff698924958efca1dc5942da23486
Yumiko Huang <dayumikoda@gmail.com>
2015-04-03 00:48:58 -0700

    append a marker right after report generated

--- public/js/current_location.js
+++ public/js/current_location.js
@@ -7,7 +7,7 @@ $(function() {
 	    current_location_map = L.mapbox.map('current_location_map', 'yumiko.lkbilml1'),
 	    myLayer = L.mapbox.featureLayer().addTo(current_location_map);
 
-	current_location_map.setView([37.734585, -122.447214], 12);
+	    current_location_map.setView([37.734585, -122.447214], 12);
 
 	// marker generating function
 	var markerGenerator = function(lat, lng, report_issue, report_id, issue, status) {
@@ -54,6 +54,72 @@ $(function() {
 		marker.addTo(current_location_map);
 	};
 
+		var user_current_lat = $("#user_current_lat"),
+		  user_current_lng = $("#user_current_lng"),
+		  create_report = $("#create_a_report"),
+		  formData = '';
+
+	// Declare the mapbox access token
+	
+	var getUserGeoinfo = function(lat, lng) {
+		// set user current lat + lng based on retrieved user geolocation info
+		user_current_lat.val(lat);
+		user_current_lng.val(lng);
+		// the lat + lng info is now in the formData, so serialize the formData now
+		formData = create_report.serialize();
+
+  	var ajaxRequest = $.ajax({
+				url: '/reports/create',
+				type: 'POST',
+	    	dataType: 'JSON',
+	    	data: formData
+		});
+		ajaxRequest.done(function(response) {
+			$('.welcome_section').append('<div class="text-danger">Thank you for sending us a report!</div>');
+			// form clear after submission
+			$('form').find("input[type=text], textarea").val("");
+			markerGenerator(response.data.report.lat, response.data.report.lng, response.data.report.report_name, response.data.report.id, response.data.report.report_type, response.data.report.status);
+			$('#geolocate.ui-button').fadeOut('slow');
+		});
+		ajaxRequest.fail(function(error) {
+			console.log('fail');
+		});
+	};
+
+	// When report is submitted:
+	$('#reportSubmit').on('click', function(event) {
+		event.preventDefault();
+		// Capture address param...
+		var report_address = $("#report_address").val();   
+		formData = create_report.serialize();
+
+		// ...and as long as it wasn't blank, do...
+		if (report_address !== ''){
+			var geocoder = L.mapbox.geocoder('mapbox.places');
+			// geocoder.query() takes a queryString (an address in this case), and a callback function into which the result of running geocoder.query() will be put.
+			geocoder.query(report_address, getJsonDataFromMapBox);
+			// getJsonDataFromMapBox() takes an error (if any), and the JSON data from running geocoder.query(). That data has several things, including a latlng array ([lat, lng]), which we want.
+			function getJsonDataFromMapBox(err, data) {
+				// Execute getUserGeoinfo function/ajax request
+			  getUserGeoinfo(data.latlng[1], data.latlng[0]);
+		  }
+		} else {
+			$.ajax({
+				url: '/reports/create',
+				type: 'POST',
+			    dataType: 'JSON',
+			    data: formData
+			}).done(function(response){
+				$('.welcome_section').append('<div class="text-danger">Thank you for sending us a report!</div>');
+				$('form').find("input[type=text], textarea").val("");
+				markerGenerator(response.data.report.lat, response.data.report.lng, response.data.report.report_name, response.data.report.id, response.data.report.report_type, response.data.report.status);
+				$('#geolocate.ui-button').fadeOut('slow');
+			}).fail(function(error){
+				console.log('fail');
+			});
+		}
+	});
+
 	// function which gets collection of reports via ajax call and executes iterateReports on it
   	var list_of_reports = function() {
 		$.ajax({
@@ -74,7 +140,7 @@ $(function() {
 	};
 
 	// execute the list_of_reports function
-  	list_of_reports();
+  list_of_reports();
 
 	if (!navigator.geolocation) {
 	    geolocate.html('Geolocation is not available');
